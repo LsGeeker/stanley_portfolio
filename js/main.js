0efc472fdc37a0e62664588732b00fdb592baf9e
KarlEaubed <carlobeddorvil20@gmail.com>
2024-10-07 13:05:42 -0400

    Mise à jour du portfolio

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+    window.addEventListener("load", function () {
+        // Masquer le loader une fois que la page est complètement chargée
+        var loaderWrapper = document.getElementById("loader-wrapper");
+        loaderWrapper.style.display = "none";
+    });
+
